d6a3153086a44e6fbc3b3cec7520b1902300ea58
Wenxu Li <wenxu@Juliens-iMac.fritz.box>
2022-02-14 19:10:05 +0100

    visual tool

--- EA_search/EA_search.py
+++ EA_search/EA_search.py
@@ -3,14 +3,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from pkg_resources import resource_filename
 import yaml
 from sklearn_genetic import GAFeatureSelectionCV
-import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
-import pandas as pd
 from sklearn.metrics import f1_score
-from sklearn.svm import SVC
 
 
 def get_config(path: str) -> dict:
@@ -42,12 +40,12 @@ def ea(neg_path, pos_path):
     X_test = np.array(X_test)
     y_train = np.array(y_train)
     y_test = np.array(y_test)
-    # clf = DecisionTreeClassifier(max_depth=6, random_state=0)
+    clf = DecisionTreeClassifier(max_depth=6, random_state=0)
     # clf = RandomForestClassifier(max_depth=2, random_state=0)
-    clf = SVC(gamma='auto', cache_size=500)
-    evolved_estimator = GAFeatureSelectionCV(estimator=clf, cv=3, scoring=None, population_size=50, generations=80,
+    # clf = SVC(gamma='auto', cache_size=500)
+    evolved_estimator = GAFeatureSelectionCV(estimator=clf, cv=3, scoring=None, population_size=10, generations=80,
                                              crossover_probability=0.8, mutation_probability=0.2, tournament_size=3,
-                                             elitism=True, max_features=100, verbose=True, keep_top_k=1,
+                                             elitism=True, max_features=None, verbose=True, keep_top_k=1,
                                              criteria='max', algorithm='eaMuPlusLambda', refit=True, n_jobs=1,
                                              pre_dispatch='2*n_jobs', return_train_score=False,
                                              log_config=None)
